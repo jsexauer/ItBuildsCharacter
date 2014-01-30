85f16a5017fa6922cd4742702aaa37075a81fec5
Jason Sexauer <genericcarbonlifeform@gmail.com>
2014-01-29 21:37:59 -0500

    Added auditability and additional character details

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 
-import random
+import random, sys
+from pprint import pprint
+from StringIO import StringIO
 random.seed()
 
 intOrNone = lambda x: None if x is None or x == '' else int(x)
@@ -235,59 +237,289 @@ def roll_d(dice):
     return random.randrange(1,dice+1,1)
 
 
-def _makeMod(attr):
+
+
+#########################################################################
+
+
+def auditable(func):
+    """
+    An auditable property.  You should define _formula property in your function
+    if not just summing everything together.
+    http://stackoverflow.com/questions/9186395/python-is-there-a-way-to-get-a-local-function-variable-from-within-a-decorator
+    http://code.activestate.com/recipes/577283-decorator-to-expose-local-variables-of-a-function-/
+    http://stackoverflow.com/questions/4357851/creating-or-assigning-variables-from-a-dictionary-in-python
+
+    """
+    def auditableFuncFactory(child_self, *args, **kwargs):
+        if not hasattr(child_self, 'audit'):
+            raise ValueError("auditble wrapper must be on auditable object")
+
+        class Tracer(object):
+            def __init__(self):
+                self.locals = {}
+            def __call__(self, frame, event, arg):
+                if event=='return':
+                    self.locals = frame.f_locals.copy()
+
+        if child_self.audit:
+            # tracer is activated on next call, return or exception
+            tracer = Tracer()
+            old_profiler =  sys.getprofile()
+            sys.setprofile(tracer)
+            try:
+                # trace the function call
+                res = func(child_self, *args, **kwargs)
+            finally:
+                # disable tracer and replace with old one
+                sys.setprofile(old_profiler)
+
+            # Filter out variables we don't want (marked with _)
+            v = tracer.locals
+            v.pop('self')
+            formula = None
+            for k in v.keys():
+                if k == '_formula':
+                    formula = v[k]
+                if k[0] == '_':
+                    v.pop(k)
+            return AuditResult(formula, res, v)
+        else:
+            return func(child_self, *args, **kwargs)
+    return property(auditableFuncFactory)
+
+
+def has_sum(list, key):
+    """Returns objects with key and the sum of those keyed values"""
+    return ( filter(lambda x: x[key]!=0, list),
+             sum(map(lambda x: x[key], list)) )
+
+def has_max(list, key):
+    """Returns objects with key and the max of those keyed values
+       (used for buffs that do not stack"""
+    return ( filter(lambda x: x[key]!=0, list),
+             max(map(lambda x: x[key], list) + [0,]) )
+
+def _makeMod(_attr):
     def _calcMod(self):
-        return (self[attr+'Score'] - 10) // 2
+        _formula = '(score-10)/2 where'
+        score = self[_attr+'_score']
+        return (score - 10) // 2
     return _calcMod
 
-def _makeScore(attr):
-    attr = attr+'Score'
+def _makeScore(_attr):
+    _attr_score = _attr+'_score'
     def _calcAttrScore(self):
-        return (self.base[attr] + self.race[attr] +
-                sum(map(lambda x: x[attr], self.buffs)) )
+        base = self.base[_attr_score]
+        racial = self.race[_attr_score]
+        buffs, _buffs = has_sum(self.buffs, _attr_score)
+        equipment, _eqp = has_sum(self.equipment, _attr_score)
+        return (base+ racial + _buffs + _eqp)
     return _calcAttrScore
+
 
 class AttributesMeta(type):
     def __new__(cls, name, bases, attrdict):
-        _attrs = attrdict.get('_attrs', {})
-        for k in _attrs:
-            attrdict[k] = property(_makeMod(k))
+        _abilities = attrdict.get('_abilities', {})
+        for k in _abilities:
+            attrdict[k] = auditable(_makeMod(k))
 
+        _scores = attrdict.get('_scores', {})
+        for k in _scores:
+            attrdict[k] = 0
         return super(AttributesMeta, cls).__new__(cls, name, bases, attrdict)
 
 class Attributes(Struct):
     __metaclass__ = AttributesMeta
-    _attrs = ['str', 'dex', 'con', 'int', 'wis', 'cha']
+    _abilities = ['str', 'dex', 'con', 'int', 'wis', 'cha']
+    _scores = ['AC', 'natural_armor', 'deflection_bonus', 'dodge_bonus']
 
     def __init__(self, default_value=0):
-        for k in self._attrs:
-            self[k+'Score'] = default_value
+        for k in self._abilities:
+            self[k+'_score'] = default_value
+        self.audit = False
 
     def __add__(self, other):
         assert isinstance(other, Attributes)
         added = Attributes()
-        for attr in self._attrs:
-            added[attr+'Score'] = self[attr+'Score'] + other[attr+'Score']
+        for attr in self._abilities:
+            added[attr+'_score'] = self[attr+'_score'] + other[attr+'_score']
         return added
+
+    def __str__(self):
+        # Display only non-zero values of self
+        s = []
+        displayable = (self._abilities + self._scores +
+                        [a+'_score' for a in self._abilities])
+        for k, v in self.__dict__.iteritems():
+            if k not in displayable:
+                continue
+            if v != 0:
+                s.append("{0:s}: {1:+d}".format(k,v))
+        return '(%s)' % ', '.join(s)
+
 
 class Race(Attributes):
     pass
 
-
+class Equipment(Attributes):
+    def __init__(self, name):
+        Attributes.__init__(self)
+        self.name = name
+    def __str__(self):
+        return '%s %s' % (self.name, Attributes.__str__(self))
 
 class CharacterMeta(AttributesMeta):
     def __new__(cls, name, bases, attrdict):
-        _attrs = Attributes._attrs
-        for k in _attrs:
-            attrdict[k+'Score'] = property(_makeScore(k))
+        _abilities = Attributes._abilities
+        for k in _abilities:
+            attrdict[k+'_score'] = auditable(_makeScore(k))
 
         return super(CharacterMeta, cls).__new__(cls, name, bases, attrdict)
 
 class Character(Attributes):
     __metaclass__ = CharacterMeta
     def __init__(self):
-        super(Attributes, self).__init__()
+        self.audit = False
         self.base = Attributes(10)
         self.race = Race()
         self.buffs = []
+        self.equipment = []
+        self.size_mod = 0
 
+    @auditable
+    def AC(self):
+        """Armor Class"""
+        base = 10
+        equipment, _eqp = has_sum(self.equipment, 'AC')
+        dex = self.dex
+        natural_armor = self.natural_armor
+        deflection_bonus = self.deflection_bonus
+        dodge_bonus = self.dodge_bonus
+        size_mod = self.size_mod
+        return (base + _eqp + dex + natural_armor + deflection_bonus +
+                  dodge_bonus + size_mod)
+
+    @auditable
+    def touch_AC(self):
+        base = 10
+        dex = self.dex
+        deflection_bonus = self.deflection_bonus
+        dodge_bonus = self.dodge_bonus
+        size_mod = self.size_mod
+        return (base + _eqp + dex + natural_armor + deflection_bonus +
+                  dodge_bonus + size_mod)
+
+    @auditable
+    def flatfooted_AC(self):
+        # Everything except dex
+        base = 10
+        equipment, _eqp = has_sum(self.equipment, 'AC')
+        natural_armor = self.natural_armor
+        deflection_bonus = self.deflection_bonus
+        dodge_bonus = self.dodge_bonus
+        size_mod = self.size_mod
+        return (base + _eqp + dex + natural_armor + deflection_bonus +
+                  dodge_bonus + size_mod)
+
+    @auditable
+    def deflection_bonus(self):
+        # Deflection bonuses do not stack
+        _formula = "max(buffs, equpment)"
+        buffs, _buffs = has_max(self.buffs, 'deflection_bonus')
+        equipment, _eqp = has_max(self.equipment, 'deflection_bonus')
+        return max(_buffs, _eqp)
+
+    @auditable
+    def dodge_bonus(self):
+        # Dodge bonus _does_ stack
+        buffs, _buffs = has_max(self.buffs, 'dodge_bonus')
+        equipment, _eqp = has_max(self.equipment, 'dodge_bonus')
+        return _buffs + _eqp
+
+class AuditResult(object):
+    _display_order = (['base', ] + Attributes._abilities
+                      + [a+'_score' for a in Attributes._abilities]
+                      + ['equipment','buffs']
+                      + Attributes._scores)
+    def __init__(self, formula, value, variables):
+        if formula is None:
+            self.formula = "Sum of"
+        else:
+            self.formula = formula
+        self.value = value
+        self.variables = variables.copy()
+    def show(self, fullTrace = True):
+        spacer = '  '
+        variables = self.variables.copy()
+
+        def get_displayworthy(v):
+            # Skip any values not worth listing
+            if not fullTrace:
+                if type(v) is int and v == 0:
+                    return ''
+                elif type(v) is list and v == []:
+                    return ''
+                elif isinstance(v, AuditResult) and v.value == 0:
+                    return ''
+
+
+            s = spacer + k + ': '
+            if isinstance(v, AuditResult):
+                indent = len(s)
+                s += '<' + str(v).replace('\n', '\n'+' '*indent)
+                s = s.rstrip() + '>'
+            elif type(v) is list:
+                for i in v:
+                    s += '\n' + spacer*2 + str(i).replace('\n', '\n'+spacer*2)
+                s = s.rstrip()
+            else:
+                s += str(v)
+            s += '\n'
+            return s
+
+        s = self.formula + ':\n'
+        # Grab keys in display order
+        for k in self._display_order:
+            v = variables.pop(k, None)
+            if v is None:
+                continue
+            s += get_displayworthy(v)
+
+        # Grab any remaining keys
+        for k, v in variables.iteritems():
+            s += get_displayworthy(v)
+        s = s.rstrip() + '\n' + '= ' + str(self.value)
+        return s
+    def __add__(self, other):
+        return other + self.value
+    def __radd__(self, other):
+        return other + self.value
+    def __sub__(self, other):
+        return self.value - other
+    def __rsub__(self, other):
+        return other - self.value
+    def __mul__(self, other):
+        return other*self.value
+    def __rmul__(self, other):
+        return other*self.value
+    def __div__(self, other):
+        return self.value/other
+    def __rdiv__(self, other):
+        return other/self.value
+    def __str__(self):
+        return self.show(False)
+
+
+
+
+
+c = Character()
+c.audit = True
+c.base.dex_score = 13
+print c.AC
+chain_shirt = Equipment('Chain Shirt')
+#chain_shirt.AC = 6
+chain_shirt.dex_score = 3
+c.equipment.append(chain_shirt)
+print c.AC
